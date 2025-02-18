75d4751fbcee4519ef6e04a1cfa35c1d0627cf91
Grace Bero <graceb620@gmail.com>
2025-02-18 14:32:26 -0600

    explore income_src

---
+++ data_exploration/income_src.R
@@ -0,0 +1,37 @@
+rm(list=ls())
+
+library(lubridate)
+library(tidyverse)
+library(ggplot2)
+
+# Create the main dataset from raw csv -----------------------------------------
+all <- read.csv("Data/drake_export_v8_2024-02-13_100754_rev2_nolatlong.csv")
+
+# Cleaning of dates ------------------------------------------------------------
+all <- all %>% 
+  mutate(
+    served_date=ymd(served_date),
+    dob=ymd(dob)
+  )
+
+# Create datasets --------------------------------------------------------------
+# Create visit level dataset
+visit <- all %>% 
+  group_by(afn, served_date) %>% 
+  # in summarize, you need to really think about how to characterize a visit
+  summarise( #ADD MORE HERE
+    n_household = n(), #counts the number of rows within each afn, served_date
+    income_source = income_source
+  ) %>% 
+  mutate (
+    served_year = year(served_date),
+    served_month = month(served_date),
+    served_day_of_month = mday(served_date),
+    round_month = round_date(served_date, "month")
+  )
+
+# Explore the Income_source
+ggplot(data=visit, aes(x=income_source)) +
+  geom_bar() + coord_flip()
+
+
