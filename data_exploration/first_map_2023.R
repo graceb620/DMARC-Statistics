1284f3460741021f55703fd03e18bd4b94fbbd80
lynette Ndibalekera <lynnettevivian04@gmail.com>
2025-04-14 10:19:37 -0500

    Update first_map_2023.R

--- data_exploration/first_map_2023.R
+++ data_exploration/first_map_2023.R
@@ -24,16 +24,6 @@ zip_shapes <- zctas(cb = FALSE, state = "IA", year = 2010)
 zip_map_data <- zip_shapes %>%
   inner_join(zip_counts, by = "ZCTA5CE10")
 
-zip_towns <- data.frame(
-  ZCTA5CE10 = c("50010", "50014", "50309"),  # Example ZIP codes
-  town_name = c("Ames", "Ames", "Des Moines")  # Corresponding town names
-)
-
-# Add town names to the map data
-zip_map_data <- zip_map_data %>%
-  left_join(zip_towns, by = "ZCTA5CE10")  # Add town names
-
-
 
 # Transform to a projected coordinate system for better visualization
 zip_map_data <- st_transform(zip_map_data, crs = 3857)
@@ -47,8 +37,8 @@ iowa_state <- states(cb = TRUE) %>%
 ggplot(data = zip_map_data) +
   geom_sf(data = iowa_state, fill = "gray95", color = "black", size = 0.3) +  
   geom_sf(aes(fill = n), color = "white", size = 0.2) +  # Fill ZIP based on count
-  scale_fill_gradient(low = "lightblue", high = "darkblue", name = "Visitors") +  # Gradient fill
-  theme_minimal() +  # Minimal theme for a clean look
+  scale_fill_gradient(low = "lightblue", high = "darkblue", name = "Visitors") +  
+  theme_minimal() +  
   ggtitle("Map of First-Time Visitors of 2023 in Iowa") +
   theme(
     plot.title = element_text(hjust = 0.5, size = 16),  # Center and enlarge title
