180531aca2279dba2a13a8bab614c9f47eaaee2c
ameliaburnell <amelia.burnell@outlook.com>
2025-04-27 19:26:50 -0500

    adding variables and only 2024 analysis

--- New Data Exploration/DataSets2024.R
+++ New Data Exploration/DataSets2024.R
@@ -17,12 +17,16 @@ summary(all2) # Checking to make sure that all of the columns are in the desired
 # format and of reasonable value 
 # Noticed that the age range was from -7974.49 and 2024 i want to check the entries
 # where the age is <0 and >120
-# test <- all2 %>%
-#   group_by(clientId) %>%
-#   filter(age < 0 | age > 120)
-# There are 139 entries where the dob entry appears to have been entered incorrect
-# We decided as a group that it would be better to just remove DOB as a variable
+test <- all2 %>%
+  group_by(clientId) %>%
+  filter(age < 0 | age > 120)
 
+# For cleaning age, although some might be obvious as to how they would be changed, 
+# the original data had some people >100 years old that were "currently pre-school or younger"
+# Because of this, it will be more difficult to clean as we no longer trust the original to be correct
+# and we are unsure what assumptions should be made
+
+#decision for age: do not include column. can use education instead
 
 ### --- Create a visit level data set -----------------------------------------
 visit2 <- all2 %>% 
@@ -40,7 +44,7 @@ visit2 <- all2 %>%
     year = year(servedDate),
     month = month(servedDate),
     day_of_month = mday(servedDate),
-    round_month = round_date(servedDate, "month").
+    round_month = round_date(servedDate, "month"),
     round_quarter = round_date(servedDate, "quarter")
   )
 
@@ -89,7 +93,7 @@ individuals <- all2 %>%
     incomeSource = first(incomeSource)
   )
 
-### --- Create a household level dataset ---------------------------------------
+### --- Create a household level dataset: Grace ---------------------------------------
 hh_data2 <- all2 %>% 
   group_by(houseHoldIdAfn) %>% 
   summarize(
@@ -122,12 +126,59 @@ hh2_2024 <- hh_data2 %>%
   select(householdMembers, visit_count_2024, IncomeSource, fedPovertyLevel,
          annualIncome, foodstamps, primary_visit_location, primary_service,
          primary_visitor_occupation, dietary_issue, veteran)
-  
+
+### --- Create a household level dataset for 2024: Amelia ---------------------------------------
+
+# General household-level summary (all years)
+hh_summary_all <- all2 %>%
+  group_by(houseHoldIdAfn) %>%
+  summarize(
+    householdMembers = max(householdMembers, na.rm = TRUE),
+    first_visit = min(servedDate, na.rm = TRUE),
+    first_visit24 = if_else(year(first_visit)==2024,1,0),
+    last_visit = max(servedDate, na.rm = TRUE)
+  ) %>% ungroup()
+
+# 2024-specific metrics
+hh_summary_2024 <- all2 %>%
+  filter(year(servedDate) == 2024) %>%
+  group_by(houseHoldIdAfn) %>%
+  summarize(
+    visit_count_2024 = n_distinct(servedDate),
+    
+    meantimeemployed = mean(incomeSource == "Employed", na.rm = TRUE),
+    unstableemployement24 = if_else(meantimeemployed<1,1,0),
+    #if there is not at least one employed person at all times, then meantime will be less than one
+    
+    medfedPovertyLevel = median(fedPovertyLevel),
+    medannualIncome = median(annualIncome),
+    
+    proportiontimeonfoodstamps = mean(foodstamps == "Yes", na.rm = TRUE), #foodstamp value the same across household
+    foodstampschange = as.integer(n_distinct(foodstamps) > 1), #if there are more than 1 distinct values for foodstamps - means it changed
+    foodstamps = as.integer(any(foodstamps == "Yes")),
+    
+    primary_visit_location = names(which.max(table(location))),
+    primary_service = names(which.max(table(service))),
+    primary_visitor_occupation = names(which.max(table(category))),
+    
+    dietary_issue = as.integer(any(!dietaryIssue %in% c("None", "Unknown"))),
+    
+    anyveteran = as.integer(any(veteran == "Yes", na.rm = TRUE)),
+    anycollege = as.integer(any(education=="College",  na.rm = TRUE)),
+    anyschoolkid = as.integer(any(education=="Currently",  na.rm = TRUE))
+  ) %>% ungroup()
+
+# Merge both
+hh_data24 <- hh_summary_all %>%
+  left_join(hh_summary_2024, by = "houseHoldIdAfn") %>% filter(year(last_visit) == 2024)
+
+
 ### --- Create CSV's -----------------------------------------------------------
 write.csv(hh_data2, "Data/hh_data2.csv", row.names = FALSE)
 write.csv(individuals, "Data/individual.csv", row.names=FALSE)
 write.csv(visit2, "Data/visit2.csv", row.names=FALSE)
 write.csv(hh2_2024, "Data/hh2_2024.csv", row.names=FALSE)
+write.csv(hh_data24, "Data/amelia_hh2_2024.csv", row.names=FALSE)
 write.csv(monthly_count2, "Data/monthly_count2.csv", row.names = FALSE)
 write.csv(quarter_count2, "Data/quarter_count2.csv", row.names = FALSE)
 write.csv(hh2_2024, "Data/hh2_2024", row.names=FALSE)
